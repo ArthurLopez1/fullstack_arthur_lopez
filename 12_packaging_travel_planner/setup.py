32978f037cebd37f480acfbece0cbd2428ee3e3e
Arthur Lopez <arthur.lopez@outlook.com>
2025-01-21 22:50:59 +0100

    packaging and testing with streamlit

--- 12_packaging_travel_planner/setup.py
+++ 12_packaging_travel_planner/setup.py
@@ -5,12 +5,13 @@ print(find_packages(exclude= ("test*", "explorations")))
 
 setup(
     name = "travel_planner",
-    version="1.0.0",
+    version="1.0.1",
     description="""
     This package is used for public transport travel planning in Sweden.
     It contains backend, frontend and util code.""",
     authour= "Arthur Lopez",
     author_email= "arthur@email.com",
     install_requires= ["pandas", "streamlit", "requests", "folium"],
-    packages = find_packages(exclude=["test*", "explorations"])
+    packages = find_packages(exclude=["test*", "explorations"]),
+    entry_points = {"console_scripts": ["dashboard = utils.run_dashboard:run_dashboard"]}
 )
